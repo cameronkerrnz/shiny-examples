826b254bde9fc921213b7484d04d3b3a072672b9
Cameron Kerr <cameron.kerr@otago.ac.nz>
2019-01-28 15:47:46 +1300

    openshift poc

--- docker/shiny-server.openshift.sh
+++ docker/shiny-server.openshift.sh
@@ -3,6 +3,9 @@
 # The container will run with a high-numbered ethemeral UID, but
 # will have group set to root.
 #
+# The uid_entrypoint.sh script will alter the UID of the 'shiny'
+# user to suit this run.
+#
 # This, in this file, everything will be run by a non-privileged
 # user, so run stuff here, but avoid setup tasks that would require
 # root privileges.
--- docker/uid_entrypoint.sh
+++ docker/uid_entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+    # Rewrite the UID of the shiny user that the shiny package created
+    # Running inside Openshift, we don't know which UID we will run as
+    sed -i -re "s@^(shiny:x):[^:]*:(.*)@\1:$(id -u):\2" /etc/passwd
   fi
 fi
 exec "$@"
