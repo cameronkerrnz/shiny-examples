21d00b6286503cd484c435cde5262409631fed6a
Cameron Kerr <cameron.kerr@otago.ac.nz>
2019-01-28 15:51:04 +1300

    unterminated s command

--- docker/uid_entrypoint.sh
+++ docker/uid_entrypoint.sh
@@ -3,7 +3,7 @@ if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
     # Rewrite the UID of the shiny user that the shiny package created
     # Running inside Openshift, we don't know which UID we will run as
-    sed -i -re "s@^(shiny:x):[^:]*:(.*)@\1:$(id -u):\2" /etc/passwd
+    sed -i -re "s@^(shiny:x):[^:]*:(.*)@\1:$(id -u):\2@" /etc/passwd
   fi
 fi
 exec "$@"
