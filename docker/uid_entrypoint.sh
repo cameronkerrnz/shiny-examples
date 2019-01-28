4c0ff0298c9f7176bb00fcf6016c29b41d657f54
Cameron Kerr <cameron.kerr@otago.ac.nz>
2019-01-28 16:01:34 +1300

    /etc is not writable for inplace editing

--- docker/uid_entrypoint.sh
+++ docker/uid_entrypoint.sh
@@ -3,7 +3,9 @@ if ! whoami &> /dev/null; then
   if [ -w /etc/passwd ]; then
     # Rewrite the UID of the shiny user that the shiny package created
     # Running inside Openshift, we don't know which UID we will run as
-    sed -i -re "s@^(shiny:x):[^:]*:(.*)@\1:$(id -u):\2@" /etc/passwd
+    sed -re "s@^(shiny:x):[^:]*:(.*)@\1:$(id -u):\2@" /etc/passwd > /tmp/passwd
+    cat /tmp/passwd > /etc/passwd
+    rm -f /tmp/passwd
   fi
 fi
 exec "$@"
